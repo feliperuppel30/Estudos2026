eaaeb24a5e82d8c36b974572763065aec6e1b4bf
Felipe Ruppel Bueno <115996060+feliperuppel30@users.noreply.github.com>
2026-07-18 16:21:06 +0100

    estudos django

--- Django-module/blog/views.py
+++ Django-module/blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+
 from django.http import HttpResponse
 
 # Create your views here.
 def blog(request):
     print('blog')
-    return HttpResponse('blog do app')
+    return HttpResponse('blog do app1')
+
+def exemplo(request):
+    print('exemplo')
+    return HttpResponse('exemplo do app1')
--- Django-module/project1/urls.py
+++ Django-module/project1/urls.py
@@ -15,15 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog import views as blog_views
-from home import views as home_views
-
+from django.urls import include, path
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', blog_views.blog),
-    path('', home_views.home),
+    path('blog/', include('blog.urls')),
+    path('', include('home.urls')),
 ]
